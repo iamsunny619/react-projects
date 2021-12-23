17effe59d34a10b2f4fd85c1640e8491843abebd
iamsunny619 <60790066+iamsunny619@users.noreply.github.com>
2021-12-23 09:06:43 +0530

    just to test

---
+++ src/components/Testing.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Testing.css";
+const Testing = () => {
+  return (
+    <div>
+      <h1>Hello I'm test file, will be controlled by root csss</h1>
+      <h2>Hello I'm test file, will be controlled by my test csss</h2>
+      <p>
+        In Test Class, will be controlled by Index css if im blue, and if Im red
+        mean my owns Test cs, and pink if App css
+      </p>
+      <h3>hahahhahaha</h3>
+    </div>
+  );
+};
+
+export default Testing;
