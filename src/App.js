c326a41f29816232c3136181e9b1b90730c54792
iamsunny619 <60790066+iamsunny619@users.noreply.github.com>
2022-03-23 11:38:01 +0530

    not completed,, still dark mode is not wworking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
-import Testing from "./components/Testing";
-import "./App.css";
+import React, { useContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { DarkModeContext } from "./context/darkModeContext";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+import "./style/dark.scss";
 const App = () => {
+  const { darkMode } = useContext(DarkModeContext);
   return (
-    <div>
-      <p>App component</p>
-      <Testing />
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userID" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productID" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
