c17f8bdcc1e07cbebc56bbc0e973578a1f866198
iamsunny619 <60790066+iamsunny619@users.noreply.github.com>
2022-06-09 13:09:16 +0530

    still not completed only single page is i.e landing page is devloped

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
-import "./App.css";
+import Home from "./Home";
+import { Route, Switch } from "react-router";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Service from "./pages/Service";
+import Error from "./pages/Error";
+
 const App = () => {
   return (
-    <div>
-      <p>App component</p>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/contact" component={Contact}></Route>
+        <Route path="/service" component={Service}></Route>
+        <Route component={Error}></Route>
+      </Switch>
+    </>
   );
 };
 
